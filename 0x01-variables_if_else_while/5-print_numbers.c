64d03c2ff0e988ab54c310edc9647c0c93c7e94f
khelvin1 <godsfireoshiogbai@gmail.com>
2023-03-18 19:46:10 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - Entry point 
- * Description: 'print numbers of base 10'
+ * main - prints all string from 0 to 9
  * Return: Always 0
  */
 int main(void)
 {
-	int n;
-	
-	for (n = 0; n <= 9; n++)
-	{
-		printf("%d", n);
-	}
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
 	printf("\n");
+
 	return (0);
 }
