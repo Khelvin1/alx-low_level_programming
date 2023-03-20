35de802db43b9b0efae937abfd5b9930b4804584
khelvin1 <godsfireoshiogbai@gmail.com>
2023-03-20 07:43:16 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 /**
- * main - prints all combination of single digits
- * Return: Always 0
+ * main - Entry point 
+ * Return: Always 0 (success)
  */
+
+
 int main(void)
+
 {
 
-	int n;
+int d;
 
-	for (n = 48; n = 58; n++)
-	{
-		putchar(n);
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	putchar('\n');
-	return (0);
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
 }
-
+}
+putchar('\n');
+return (0);
+}
