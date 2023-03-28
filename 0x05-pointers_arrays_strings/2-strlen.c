ebaba71e6d5ee89c8dff41f052810161f837bf30
khelvin1 <godsfireoshiogbai@gmail.com>
2023-03-27 19:20:40 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,9 +8,10 @@
 size_t _strlen(const char *str)
 
 {
-        size_t length = 0;
-        
-        while (*str++)
-        length++;
-        return (length);
+size_t length = 0;
+
+while (*str++)
+length++;
+return (length);
+
 }
