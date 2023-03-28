7c94f4055caf33dfb615c83608f994d37d7be01e
khelvin1 <godsfireoshiogbai@gmail.com>
2023-03-28 10:49:26 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,10 +17,10 @@ void print_array(int *a, int n)
 	for (j = 0; j < n; j++)
 	{
 	printf("%d", a[j]);
-	if (j != (n -1))
+	if (j != (n - 1))
 	{
-	printf(";");
+	printf(", ");
 	}
 	}
 	printf("\n");
-}`
+}
