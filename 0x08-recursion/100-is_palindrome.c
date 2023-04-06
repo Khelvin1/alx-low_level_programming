8ca226147f0716b528fe5d6a1afc8a83212e245c
khelvin1 <godsfireoshiogbai@gmail.com>
2023-04-06 15:19:38 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,7 +26,8 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return (1 + _strlen_recursion(s + 1));
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
